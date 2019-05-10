93d8f384fa5ff4666387bdfdc91e5876556b0b83
africanoKevin <48529005+africanoKevin@users.noreply.github.com>
2019-05-10 11:06:37 -0700

    build file

---
+++ build/app.js
@@ -0,0 +1,17 @@
+let _express = require('express');
+
+let _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+let app = (0, _express2.default)();
+
+app.use(_express2.default.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).send({ 'message': 'congratulations your first end point is working' });
+});
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
