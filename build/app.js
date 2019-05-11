2a92d4b6c7ca1b5dce9e86a06d9233cd06c2f8fe
africanoKevin <48529005+africanoKevin@users.noreply.github.com>
2019-05-10 23:28:17 -0700

    [chore] update build file

--- build/app.js
+++ build/app.js
@@ -5,13 +5,13 @@ let _express2 = _interopRequireDefault(_express);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 let app = (0, _express2.default)();
+const port = process.env.PORT || 4000;
 
 app.use(_express2.default.json());
 
-app.get('/', (req, res) => {
-    return res.status(200).send({ 'message': 'congratulations your first end point is working' });
-});
+app.get('/', (req, res) =>
+        res.status(200).send({ message: 'congratulations your first end point is working' });
 
-app.listen(4000, () => {
-    console.log('server is running on port 4000');
-});
+        app.listen(port, () => {
+            console.log('server is running on port 4000');
+        });
