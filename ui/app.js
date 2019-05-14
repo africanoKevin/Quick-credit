4e400c0debf41078b5a46237b11c3b80c94404a4
africanoKevin <48529005+africanoKevin@users.noreply.github.com>
2019-05-14 06:55:09 -0700

    [chore] update js file

--- ui/app.js
+++ ui/app.js
@@ -1,11 +1,14 @@
-document.getElementById('date').innerHTML=new Date().toDateString()
+document.getElementById('date').innerHTML = new Date().toDateString();
+document.getElementById('date2').innerHTML = new Date().toDateString();
+document.getElementById('date3').innerHTML = new Date().toDateString();
+document.getElementById('date4').innerHTML = new Date().toDateString()
 
-function fFunction(){
-  var a= document.getElementById('fname')
-  a.value=a.value.toUpperCase();
+function fFunction() {
+    var a = document.getElementById('fname')
+    a.value = a.value.toUpperCase();
 }
 
-function lFunction(){
-  var a= document.getElementById('lname')
-  a.value=a.value.toUpperCase();
+function lFunction() {
+    var a = document.getElementById('lname')
+    a.value = a.value.toUpperCase();
 }
