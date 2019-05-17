577c06a2a658f18315c204ee7c6b57255642db4b
africanoKevin <48529005+africanoKevin@users.noreply.github.com>
2019-05-17 08:05:06 -0700

    Delete master-route.js

--- server/routes/master-route.js
+++
@@ -1,9 +0,0 @@
-import userRoutes from './user/user-route';
-import loanRoute from './loan/loan-route';
-
-const routeMaster = (app) => {
-    userRoutes(app);
-    loanRoute(app);
-};
-
-export default routeMaster;
