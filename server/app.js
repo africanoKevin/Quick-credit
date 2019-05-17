e2af3c168ba2d4b7401d29a7056f2ac87ad4b770
africanoKevin <48529005+africanoKevin@users.noreply.github.com>
2019-05-16 23:20:59 -0700

    [chore] update app.js

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import masterRoute from './routes/masterRoute';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+masterRoute(app);
+
+app.listen(port, () => {
+    ('server is running on port 3000 ');
+});
+export default app;
